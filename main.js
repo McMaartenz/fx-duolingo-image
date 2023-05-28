d6a187390eb03d9bcb3eb80485b34a80362e1130
Maarten van Keulen <mcmaartenz@outlook.com>
2023-05-28 22:52:05 +0200

    Wiktionary

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const DuoImgSearchExt = (() => {
+	const imageApi = 'https://loremflickr.com/320/240/';
+
 	const getDOM = () => {
 		return document.querySelector('div[data-test="challenge challenge-translate"]');
 	};
@@ -11,7 +13,7 @@ const DuoImgSearchExt = (() => {
 		const div = document.createElement('div');
 		
 		Object.assign(div.style, {
-			width: '100%',
+			width: '337px',
 			height: '100%',
 			gridColumn: 2,
 			display: 'flex',
@@ -20,24 +22,50 @@ const DuoImgSearchExt = (() => {
 			overflowY: 'scroll',
 			maxHeight: '600px',
 			borderRadius: '10px',
+			overflowX: 'hidden',
+			scrollbarWidth: 'thin',
 		});
 
-		div.setAttribute('duo-img-search','');
+		var css = `
+		div[duo-img] {
+			opacity: 1;
+		}
+		div[duo-img]:hover {
+			opacity: 0;
+			z-index: -1;
+		}
+		iframe:hover {
+			z-index:99!important;
+		}
+		`;
 
+		var style = document.createElement('style');
+
+		if (style.styleSheet) {
+			style.styleSheet.cssText = css;
+		} else {
+			style.appendChild(document.createTextNode(css));
+		}
+
+		div.setAttribute('duo-img-search','');
 		const dom = getDOM();
 		if (dom === null) {
-			setTimeout(inject, 2000);
+			setTimeout(inject, 4000);
 			console.log('[DuoImg]: Trying to reinject..');
 			return;
 		}
+
+		dom.appendChild(style);
 
 		Object.assign(dom.style, {
 			gridTemplateColumns: 'max-content',
 			gridColumnGap: '2%',
 		});
 
+		console.log('[DuoImg]: Injecting..');
 		dom.appendChild(div);
 		addListener();
+		update();
 	};
 
 	const selectElement = () => {
@@ -47,7 +75,6 @@ const DuoImgSearchExt = (() => {
 	const selectOrInject = () => {
 		const el = selectElement();
 		if (el === null) {
-			console.log('[DuoImg]: Injecting..');
 			inject();
 		}
 
@@ -61,28 +88,54 @@ const DuoImgSearchExt = (() => {
 	};
 
 	const createImg = (word) => {
+		const entry = document.createElement('div');
+		Object.assign(entry.style, {
+			position: 'relative',
+			minHeight: '240px',
+		});
+
+		const card = document.createElement('iframe');
+		card.src = `https://en.m.wiktionary.org/wiki/${word}`;
+
+		Object.assign(card.style, {
+			position: 'absolute',
+			left: '0',
+			border: 'none',
+			width: '320px',
+			height: '100%',
+			borderRadius: '10px',
+		});
+
 		const img = document.createElement('div');
 		img.innerText = word;
+		img.setAttribute('duo-img','');
 
 		Object.assign(img.style, {
 			textShadow: '-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000',
 			display: 'flex',
 			justifyContent: 'center',
 			alignItems: 'end',
-			background: `url(https://loremflickr.com/320/240/${word})`,
+			background: `url(${imageApi}${word})`,
 			backgroundSize: 'contain',
 			backgroundRepeat: 'no-repeat',
 			aspectRatio: '1.33 / 1',
 			borderRadius: '10px',
 			minWidth: '320px',
-		  });
+			color: 'white',
+			position: 'absolute',
+		});
 
-		return img;
+		entry.appendChild(card);
+		entry.appendChild(img);
+		return entry;
+	}
+
+	const getButton = () => {
+		return document.querySelector('[data-test=player-next]');
 	}
 
 	const addListener = () => {
-		const button = document.querySelector('[data-test=player-next]');
-		
+		const button = getButton();
 		const updater = () => {
 			try {
 				setTimeout(update, 500);
@@ -101,16 +154,25 @@ const DuoImgSearchExt = (() => {
 		imgs.innerHTML = '';
 
 		console.log('[DuoImg]: Updating..');
-		for (const word of getWords()) {
+
+		const words = getWords();
+		console.log('[DuoImg]: Rendering for words: ' + words);
+		for (const word of words) {
 			imgs.appendChild(
 				createImg(word)
 			);
 		}
 	};
 
+	const addCheck = () => {
+		console.log('[DuoImg]: Checking if still there..');
+		selectOrInject();
+		setTimeout(addCheck, 2000);
+	}
+
 	const init = () => {
-		update();
-		addListener();
+		inject();
+		addCheck();
 	};
 
 	return {
